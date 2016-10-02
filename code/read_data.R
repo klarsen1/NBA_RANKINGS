3237e86a4357b271d365d3ce4e0cbd84abd2562d
Kim Larsen <kimlarsen@Kim-Larsens-2015-MacBook-Pro.local>
2016-10-02 16:21:37 -0700

    adding future games

--- code/read_data.R
+++ code/read_data.R
@@ -12,10 +12,9 @@ source("/Users/kimlarsen/Documents/Code/NBA_RANKINGS/functions/distance_between.
 
 setwd("/Users/kimlarsen/Documents/Code/NBA_RANKINGS/rawdata/")
 
+
 ## Read the raw data
 read_player_data <- function(season, first_labels, suffix){
-  #data <- data.frame(read_excel(paste0(season, "-Player-BoxScore-Dataset.xlsx"), sheet=1))
-  #meta <- data.frame(read_excel(paste0(season, "-Player-BoxScore-Dataset.xlsx"), sheet=2, col_names = FALSE))
   data <- data.frame(read_excel(paste0("s", suffix, ".xlsx"), sheet=1))
   meta <- data.frame(read_excel(paste0("s", suffix, ".xlsx"), sheet=2, col_names = FALSE))
   labels <- c(first_labels, meta$X1)
@@ -46,8 +45,6 @@ s2 <- read_player_data("NBA-2013-2014", c("SEASON", "DATE", "PLAYER FULL NAME", 
 s3 <- read_player_data("NBA-2014-2015", c("SEASON", "DATE", "PLAYER FULL NAME", "POSITION"), 3)
 s4 <- read_player_data("NBA-2015-2016", c("SEASON", "DATE", "PLAYER FULL NAME", "POSITION"), 4)
 
-setwd("/Users/kimlarsen/Documents/Code/NBA_RANKINGS/cleandata")
-
 ## Add some indicators
 f <- rbind.data.frame(s1, s2, s3, s4) %>%
      mutate(home_team=as.numeric(VENUE_R_H=='H'), 
@@ -57,8 +54,44 @@ f <- rbind.data.frame(s1, s2, s3, s4) %>%
             playoff_minutes=playoffs*minutes,
             playoff_points=playoffs*points,
             DATE=as.Date(DATE, format="%m/%d/%Y"),
-            quarter=quarter(DATE), 
-            row_id=row_number())
+            quarter=quarter(DATE),
+            future_game=0)
+
+max_date <- max(f$DATE)
+
+## Read the schedule
+schedule <- data.frame(read_excel("schedule.xlsx", sheet=1))
+team_map <- data.frame(read_excel("schedule.xlsx", sheet=2)) %>% 
+  select(City, NBAstuffer.Initials) %>% distinct(NBAstuffer.Initials, .keep_all=TRUE)
+
+home <- rename(schedule, NBAstuffer.Initials=HOME) %>% 
+  inner_join(team_map, by="NBAstuffer.Initials") %>%
+  rename(home_team=City) %>%
+  mutate(DATE=as.Date(Date, format="%m/%d/%Y")) %>%
+  select(home_team)
+
+road <- rename(schedule, NBAstuffer.Initials=ROAD) %>% 
+  inner_join(team_map, by="NBAstuffer.Initials") %>%
+  rename(road_team=City) %>% 
+  mutate(DATE=as.Date(Date, format="%m/%d/%Y")) %>%
+  select(road_team, DATE)
+
+set.seed(2015)
+
+future_schedule <- data.frame(cbind(home, road)) %>% filter(DATE>max_date) %>%
+  mutate(r=runif(n()),
+         future_game=1,
+         OWN_TEAM=ifelse(r>0.5, home_team, road_team),
+         OPP_TEAM=ifelse(OWN_TEAM==home_team, road_team, home_team),
+         VENUE_R_H=ifelse(OWN_TEAM==home_team, 'H', 'R'), ###### FIX
+         DATA_SET="2-16-2017 Regular Season") %>%
+  select(DATE, OWN_TEAM, OPP_TEAM, VENUE_R_H, DATA_SET, future_game)
+  
+f <- bind_rows(f, future_schedule) %>%
+  replace(is.na(.), 0)
+  
+
+setwd("/Users/kimlarsen/Documents/Code/NBA_RANKINGS/cleandata")
 
 ## Create an ID
 f$cat <- paste0(f$OWN_TEAM, f$OPP_TEAM)
@@ -67,8 +100,7 @@ f$game_id <- paste0(f$DATE, vapply(stri_split_boundaries(f$cat, type = "characte
 f$cat <- NULL
 
 ## Team/game level points
-team_pts <- f %>% 
-            group_by(game_id, OWN_TEAM, VENUE_R_H, DATE) %>%
+team_pts <- group_by(f, game_id, OWN_TEAM, OPP_TEAM, VENUE_R_H, DATE, future_game) %>%
             summarise(total_playoff_minutes=sum(minutes*playoffs),
                       total_playoff_points=sum(points*playoffs),
                       total_minutes=sum(minutes), 
@@ -77,8 +109,7 @@ team_pts <- f %>%
             ungroup()
 
 ## Game level points
-game_pts <- team_pts %>% 
-            group_by(game_id) %>%
+game_pts <- group_by(team_pts, game_id, future_game) %>%
             mutate(home_team_points=total_points*(VENUE_R_H=='H'), 
                    road_team_points=total_points*(VENUE_R_H=='R')) %>%
             summarise(max_game_points=max(total_points), 
@@ -86,7 +117,6 @@ game_pts <- team_pts %>%
                       road_team_points=sum(road_team_points)) 
 
 ## Random indicator to choose the selected team
-set.seed(2015)
 game_pts$r <- as.numeric(rbinom(nrow(game_pts), 1, 0.5)>0.5)
 
 ## Create win indicators at the game/team level            
@@ -107,17 +137,26 @@ names(city_lat_long) <- c("lon","lat","OWN_TEAM")
 
 
 ## Create win indicators at the game level
-game_win <- team_win %>% group_by(game_id, DATE) %>%
+game_win <- group_by(team_win, game_id, DATE, future_game) %>%
             mutate(selected_team_win=ifelse(r==1, win*(VENUE_R_H=='H'), win*(VENUE_R_H=='R'))) %>%
             summarise(selected_team_win=max(selected_team_win),
                       playoffs=max(playoffs)) %>%
             ungroup()
 
 ## Create a game level summary file to be saved
+future_flipped <- filter(team_win, future_game==1) %>%
+  mutate(VENUE_R_H2=ifelse(VENUE_R_H=='H', 'T', ifelse(VENUE_R_H=='T', 'H')),
+         OWN_TEAM2=OPP_TEAM, 
+         OPP_TEAM2=OWN_TEAM) %>%
+  select(-OPP_TEAM, -OWN_TEAM) %>%
+  rename(OPP_TEAM=OPP_TEAM2, 
+         OWN_TEAM=OWN_TEAM2)
+  
+
 split <- split(team_win, team_win$game_id)
 game_scores <- data.frame(rbindlist(lapply(split, function(x) spread(select(x, game_id, VENUE_R_H, OWN_TEAM), VENUE_R_H, OWN_TEAM))), stringsAsFactors = FALSE) %>%
-                   inner_join(select(game_pts, -max_game_points), by="game_id") %>%
-                   inner_join(game_win, by="game_id") %>%
+                   inner_join(select(game_pts, -max_game_points, -future_game), by="game_id") %>%
+                   inner_join(select(game_win, -future_game), by="game_id") %>%
                    mutate(selected_team=ifelse(r==1, H, R), 
                           opposing_team=ifelse(r==1, R, H)) %>% 
                    select(-r) %>%
@@ -125,41 +164,63 @@ game_scores <- data.frame(rbindlist(lapply(split, function(x) spread(select(x, g
 saveRDS(game_scores, "GAME_SCORES.RDA")
 
 get_travel_dist <- function(team){
-  print(team)
-  df1 <- subset(game_scores, selected_team==team) %>% arrange(DATE) %>%
+  df <- subset(game_scores, selected_team==team) %>%
     mutate(OWN_TEAM=selected_team) %>%
     inner_join(city_lat_long, by="OWN_TEAM") %>%
     rename(lat1=lat, lon1=lon) %>%
-    mutate(OWN_TEAM=home_team_name) %>%
+    mutate(OWN_TEAM=opposing_team) %>%
     inner_join(city_lat_long, by="OWN_TEAM") %>%
     rename(lat2=lat, lon2=lon) %>%
-    mutate(travel=distance_between(lon1,lat1,lon2,lat2)) %>%
+    mutate(travel=ifelse(selected_team==home_team_name, -distance_between(lon1,lat1,lon2,lat2), distance_between(lon1,lat1,lon2,lat2))) %>%
     select(game_id, travel)
-  
-  df2 <- subset(game_scores, home_team_name==team | road_team_name==team) %>% arrange(DATE) %>%
-    mutate(days_since_last_game=DATE-lag(DATE), 
-           start_of_season=ifelse(days_since_last_game>14 | is.na(days_since_last_game)==TRUE, 1, 0),
-           days_since_last_game=ifelse(start_of_season==1, 0, days_since_last_game)) %>%
-    filter(selected_team==team) %>%
-    select(days_since_last_game, start_of_season, game_id)
-  
-  df3 <- inner_join(df1, df2, by="game_id") %>% mutate(travel=ifelse(start_of_season==1, 0, travel))
-  
-  return(df3)
+  return(df)
 }
 
 travel_data <- list()
-
 for (i in 1:nrow(city_lat_long)){
   travel_data[[i]] <- get_travel_dist(city_lat_long[i,]$OWN_TEAM)
 }
+travel_data <- data.frame(rbindlist(travel_data)) %>% distinct(game_id, .keep_all=TRUE)
 
-travel_data <- rbindlist(travel_data)
+get_rest_days <- function(id){
+
+  selected <- subset(game_scores, game_id==id)$selected_team
+  opposing <- subset(game_scores, game_id==id)$opposing_team
+  
+  df1 <- subset(game_scores, home_team_name==selected | road_team_name==selected) %>% 
+    arrange(DATE) %>%
+    mutate(days_since_last_game=DATE-lag(DATE), 
+           start_of_season=ifelse(days_since_last_game>14 | is.na(days_since_last_game)==TRUE, 1, 0),
+           selected_team_rest=ifelse(start_of_season==1, 0, days_since_last_game)) %>%
+    filter(game_id==id) %>%
+    select(selected_team_rest)
+  
+  df2 <- subset(game_scores, home_team_name==opposing | road_team_name==opposing) %>% 
+    arrange(DATE) %>%
+    mutate(days_since_last_game=DATE-lag(DATE), 
+           start_of_season=ifelse(days_since_last_game>14 | is.na(days_since_last_game)==TRUE, 1, 0),
+           opposing_team_rest=ifelse(start_of_season==1, 0, days_since_last_game)) %>%
+    filter(game_id==id) %>%
+    select(opposing_team_rest, game_id)
+
+  return(data.frame(cbind(df1, df2)))
+}
+
+
+rest_days <- list()
+ids <- unique(game_scores$game_id)
+for (i in 1:length(ids)){
+  rest_days[[i]] <- get_rest_days(ids[i])
+}
+rest_days <- data.frame(rbindlist(rest_days)) %>% 
+  mutate(rest_differential=selected_team_rest-opposing_team_rest) %>%
+  select(game_id, rest_differential)
 
 ## Create the fill box score file
 f <- inner_join(f, select(team_win, -DATE, -VENUE_R_H, -r, -playoffs), by=c("game_id", "OWN_TEAM")) %>%
      inner_join(select(game_scores, -DATE, -playoffs), by="game_id") %>%
      inner_join(travel_data, by="game_id") %>%
+     inner_join(rest_days, by="game_id") %>%
      mutate(share_of_minutes=minutes/total_minutes, 
             share_of_playoff_minutes=ifelse(total_playoff_minutes>0, playoff_minutes/total_playoff_minutes, 0),
             share_of_playoff_points=ifelse(total_playoff_points>0, playoff_points/total_playoff_points, 0),
--- functions/predict_game.R
+++ functions/predict_game.R
@@ -1,10 +1,46 @@
-predict_game(X, b, gamedata, team1, team2){
-  ### First get the average minutes and standard deviations for each player
-  players <- filter(gamedata, selected_team==team1 | opposing_team==team2) %>%
-    group_by(FULL_PLAYER_NAME) %>%
-    
+source("/Users/kimlarsen/Documents/Code/NBA_RANKINGS/functions/get_surplus_variables.R")
 
-  ## 
+predict_game <- function(X, b, gamedata, team1, team2, date, runs=1000){
   
+  ### First get the average minutes and standard deviations for each player
+  dist <- filter(gamedata, OWN_TEAM==team1 | OWN_TEAM==team2) %>%
+    group_by(PLAYER_FULL_NAME) %>%
+    summarise(m_share_of_minutes=mean(share_of_minutes), 
+              s_share_of_minutes=sd(share_of_minutes))
+
+  ### Pick a team
+  if (runif(1)>0.5) selected_team=team1
+  else selected_team=team2
+  
+  ## Infer active rosters
+  distactive <- filter(gamedata, (OWN_TEAM==team1 | OWN_TEAM==team2) & date-14<DATE & DATE<date) %>%
+    group_by(PLAYER_FULL_NAME) %>%
+    filter(DATE==max(DATE)) %>%
+    inner_join(dist, by="PLAYER_FULL_NAME") %>%
+    select(PLAYER_FULL_NAME, OWN_TEAM, m_share_of_minutes, s_share_of_minutes) %>%
+    group_by(OWN_TEAM) %>%
+    arrange(OWN_TEAM, m_share_of_minutes) %>%
+    mutate(player=row_number()) %>%
+    filter(player<14) %>%
+    ungroup()
+  
+  set.seed(2015)
+  samples <- list()
+  for (j in 1:runs){
+    df <- mutate(distactive, 
+                 share_of_minutes=rnorm(1, m_share_of_minutes, s_share_of_minutes), 
+                 share_of_minutes_signed = ifelse(OWN_TEAM==selected_team, share_of_minutes, -share_of_minutes)) %>%
+    
+      get_surplus_variables()  
+    
+    samples[[i]] <- df
+  }
+  
+  samplesdf <- rbindlist(samples)
+  
+  return(data.frame(distactive))
   
 }
+
+
+t=predict_game(1,2,f,"Golden State", "Cleveland", as.Date("2016-05-01"))

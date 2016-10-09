ede9a36597653b33bbf64df00b473412f6d26265
Kim Larsen <kimlarsen@Kim-Larsens-2015-MacBook-Pro.local>
2016-10-08 21:48:35 -0700

    change the win perc calc to only look within the season

--- code/run_model.R
+++ code/run_model.R
@@ -21,6 +21,8 @@ winstreak_window <- 91
 playing_time_window <- 91
 cluster_window <- 91
 alpha <- 0 # for elastic net
+sims <- 1
+ignore_winstreaks <- 0
 
 ### Start the forecast date and end dates
 start_date <- min(subset(box_scores, season==2015)$DATE)
@@ -29,39 +31,46 @@ end_date <- max(subset(box_scores, season==2015)$DATE)
 ### Cut off the box scores
 box_scores <- subset(box_scores, DATE<=end_date)
 
+
+### If we want to trick the model to backcast, edit the future_game indicator
+#box_scores <- mutate(box_scores, future_game = ifelse(DATE>as.Date(), 1, future_game))
+
+
 ### Create a date-index
 datemap <- select(box_scores, DATE, DATE_INDEX, future_game, season) %>% distinct(DATE, .keep_all=TRUE)
 
 ### specify start and end points
 ignore_season_prior_to <- 2014
 start_index <- subset(datemap, DATE==start_date)$DATE_INDEX
-end_index <- nrow(datemap)
-end_index <- start_index+5
-
-scores <- list()
-counter <- 1
-
-centroids <- readRDS("/Users/kimlarsen/Documents/Code/NBA_RANKINGS/centroids/centroids.RDA")
+end_index <- subset(datemap, DATE==end_date)$DATE_INDEX
+#end_index <- start_index+5
 
 
 ### Assign clusters to the historical data and calculate rolling win percentages
+centroids <- readRDS("/Users/kimlarsen/Documents/Code/NBA_RANKINGS/centroids/centroids.RDA")
 box_scores_plus_list <- list()
 counter <- 1
 for (i in min(subset(datemap, season==ignore_season_prior_to)$DATE_INDEX):max(subset(datemap, future_game==0)$DATE_INDEX)){
   
-  print(paste0("DATE = ", datemap[i, "DATE"]))
+  print(paste0("Assigning clusters to DATE = ", datemap[i, "DATE"]))
 
   ### Data inside the window  
-  inwindow <- filter(box_scores, DATE<datemap[i, "DATE"] & DATE>datemap[i-cluster_window, "DATE"])
-  thisdate <- filter(box_scores, DATE==datemap[i, "DATE"])
+  inwindow <- filter(box_scores, DATE_INDEX<datemap[i, "DATE_INDEX"] & DATE_INDEX>datemap[i-cluster_window, "DATE_INDEX"])
+  thisdate <- filter(box_scores, DATE_INDEX==datemap[i, "DATE_INDEX"])
+  thisseason <- thisdate[1,"season"]
 
-  win_perc <- weighted_winpercentages(filter(inwindow, DATE>datemap[i-winstreak_window, "DATE"]))
-  
+  win_perc <- weighted_winpercentages(filter(inwindow, DATE_INDEX>datemap[i-winstreak_window, "DATE_INDEX"]), thisseason)
+
   clusters <- assign_clusters(centroids, inwindow, cutoff)
-
+  
   ### Join against win percentages and clusters  
   f <- inner_join(thisdate, select(clusters, PLAYER_FULL_NAME, Cluster), by="PLAYER_FULL_NAME") %>%
-       inner_join(win_perc, by="selected_team")
+       left_join(win_perc, by="selected_team") %>%
+        replace(is.na(.), 0)
+  
+  if (ignore_winstreaks==1){
+    f$win_perc_delta <- 0
+  }
   
   box_scores_plus_list[[counter]] <- f
   counter <- counter + 1
@@ -73,50 +82,65 @@ nclus <- max(box_scores_plus$Cluster)
 
 ### Predict the past and the future
 counter <- 1
+modelupdates <- 1
+scores <- list()
+model_details <- list()
 for (i in start_index:end_index){
-  
   
   ### Make sure we only use real data
   max_real_date <- max(subset(datemap, future_game==0)$DATE_INDEX)
   j <- min(max_real_date, i)
 
   ### Check the dates
-  print(i)
-  print(j)
   print(datemap[i, "DATE"])
   print(datemap[j, "DATE"])
   
   ### Data inside the window  
-  inwindow <- filter(box_scores_plus, DATE<datemap[j, "DATE"] & DATE>datemap[j-estimation_window, "DATE"]) 
+  inwindow <- filter(box_scores_plus, DATE_INDEX<datemap[j, "DATE_INDEX"] & DATE_INDEX>datemap[j-estimation_window, "DATE_INDEX"]) 
   
-  ### Get rest and travel by game_id
-  rest_and_travel <- select(inwindow, game_id, travel, rest_differential) %>% 
-    distinct(game_id, .keep_all=TRUE)
-
-  ### Estimate the model
-  x <- get_surplus_variables(inwindow, nclus)  %>%
-    inner_join(win_perc, by="selected_team") %>%
-    inner_join(rest_and_travel, by="game_id") %>%
-    select(-game_id, -selected_team)
-
-  Y <- x$selected_team_win
-  x$selected_team_win <- NULL
-
-  X <- model.matrix(as.formula(Y ~ .), x)
-  model <- cv.glmnet(y=Y, x=X, family="binomial", alpha=alpha, parallel=FALSE, nfolds=10)
-  c <- as.matrix(coef(model, s=model$lambda.1se))
-  p <- prob_win <- 1/(1+exp(-X%*%c[-1]))
-  print(paste0("C: ", AUC(Y, p)[1]))
+  ### Estimate the model unless we have run out of historical data
+  if (counter==1 | i <= j){
   
-  ### Score the games
+     ### Get rest and travel by game_id
+     rest_and_travel <- select(inwindow, game_id, travel, rest_differential) %>% 
+       distinct(game_id, .keep_all=TRUE)
+
+     ### Estimate the model
+     x <- get_surplus_variables(inwindow, nclus)  %>%
+       inner_join(win_perc, by="selected_team") %>%
+       inner_join(rest_and_travel, by="game_id") %>%
+       select(-game_id, -selected_team)
+
+     Y <- x$selected_team_win
+       x$selected_team_win <- NULL
+
+     X <- model.matrix(as.formula(Y ~ .), x)
+     model <- cv.glmnet(y=Y, x=X, family="binomial", alpha=alpha, parallel=FALSE, nfolds=10)
+     c <- as.matrix(coef(model, s=model$lambda.1se))
+     p <- prob_win <- 1/(1+exp(-X%*%c[-1]))
+     
+     details <- cbind.data.frame(sapply(row.names(c), as.character), sapply(c, as.numeric))
+     names(details) <- c("Variable", "Coeff")
+     details$DATE <- datemap[i, "DATE"] 
+     details$AUROC <- AUC(Y, p)[1]
+     details$N <- nrow(X)
+     model_details[[modelupdates]] <- subset(details, Variable != "(Intercept)")
+     modelupdates <- modelupdates + 1
+  }
+  
+  ### Predict game outcomes
   thisday <- filter(box_scores, DATE==datemap[i, "DATE"]) 
   games <- unique(thisday$game_id)
 
   for (d in 1:length(games)){
-    scores[[counter]] <- predict_game(c, filter(inwindow, DATE>datemap[j-playing_time_window, "DATE"]), win_perc, games[d], datemap[i, "DATE"], 100, subset(thisday, game_id==games[d]), nclus)
+    scores[[counter]] <- predict_game(c, filter(inwindow, DATE_INDEX>datemap[j-playing_time_window, "DATE_INDEX"]), win_perc, games[d], datemap[i, "DATE"], sims, subset(thisday, game_id==games[d]), nclus)
     counter <- counter + 1
   }
 }
 
 output <- data.frame(rbindlist(scores))
-  
+models <- data.frame(rbindlist(model_details))
+
+print(paste0("C: ", AUC(output$selected_team_win, output$prob_selected_team_win)[1]))
+print(paste0("C: ", AUC(output$selected_team_win, output$prob_of_probs)[1]))
+
--- functions/predict_game.R
+++ functions/predict_game.R
@@ -18,28 +18,34 @@ predict_game <- function(b, history, win_perc, id, date, runs=100, tobescored, n
   ### First get the average minutes and standard deviations for each player
   dist <- filter(history, (OWN_TEAM==team1 | OWN_TEAM==team2)) %>%
     group_by(PLAYER_FULL_NAME) %>%
-    summarise(m_share_of_minutes=mean(share_of_minutes), 
+    summarise(n=sum(share_of_minutes*DATE_INDEX),
+              d=sum(DATE_INDEX),
+              m_share_of_minutes=mean(share_of_minutes),
               s_share_of_minutes=sd(share_of_minutes)) %>%
+    select(-n, -d) %>%
     replace(is.na(.), 0)
   
+  ## Apply the overrides
+  history_override <- left_join(history, overrides, by="PLAYER_FULL_NAME") %>%
+    mutate(OWN_TEAM=ifelse(is.na(NEW_TEAM)==FALSE & DATE>=as.Date(OVERRIDE_DATE, format="%m/%d/%Y"), NEW_TEAM, OWN_TEAM)) %>%
+    select(-NEW_TEAM, -OVERRIDE_DATE)
+    
   ## Infer active rosters
-  dist_active <- filter(history, (OWN_TEAM==team1 | OWN_TEAM==team2) & (season==thisseason | season==thisseason-1)) %>%
+  dist_active <- filter(history_override, (OWN_TEAM==team1 | OWN_TEAM==team2) & (season==thisseason | season==thisseason-1)) %>%
     group_by(PLAYER_FULL_NAME) %>%
     filter(DATE==max(DATE)) %>%
     inner_join(dist, by="PLAYER_FULL_NAME") %>%
     select(PLAYER_FULL_NAME, OWN_TEAM, m_share_of_minutes, s_share_of_minutes, Cluster) %>%
-    left_join(overrides, by="PLAYER_FULL_NAME") %>%
     group_by(OWN_TEAM) %>%
     arrange(OWN_TEAM, m_share_of_minutes) %>%
-    mutate(player=row_number()) %>%
-    ungroup() %>%
-    mutate(OWN_TEAM=ifelse(is.na(NEW_TEAM)==TRUE, OWN_TEAM, NEW_TEAM),
+    mutate(player=row_number(),
            game_id=id, 
            selected_team=selected, 
            home_team_selected=as.numeric(selected_team==team1), 
            selected_team_win=w) %>%
     filter(player<14) %>%
-    select(-player, -NEW_TEAM)
+    select(-player) %>%
+    ungroup()
 
   set.seed(2015)
   samples <- list()
@@ -48,18 +54,25 @@ predict_game <- function(b, history, win_perc, id, date, runs=100, tobescored, n
     x$share_of_minutes=rnorm(1, x$m_share_of_minutes, x$s_share_of_minutes)
     return(x)
   }
-  for (j in 1:runs){
-    df <- data.frame(rbindlist(lapply(split(dist_active, dist_active$PLAYER_FULL_NAME), sim_share_of_minutes))) %>%
-      mutate(share_of_minutes_signed = ifelse(OWN_TEAM==selected_team, share_of_minutes, -share_of_minutes))
+  if (runs>1){
+    for (j in 1:runs){
+      df <- data.frame(rbindlist(lapply(split(dist_active, dist_active$PLAYER_FULL_NAME), sim_share_of_minutes))) %>%
+        mutate(share_of_minutes_signed = ifelse(OWN_TEAM==selected_team, share_of_minutes, -share_of_minutes))
 
-    x <- get_surplus_variables(df, nclus) %>%
-      left_join(win_perc, by="selected_team")
+      x <- get_surplus_variables(df, nclus) %>%
+        left_join(win_perc, by="selected_team")
 
-    samples[[j]] <- data.frame(cbind(x, select(thisgame, DATE, travel, rest_differential, home_team_name, road_team_name, home_team_points, road_team_points), row.names=NULL), stringsAsFactors = FALSE)
+      samples[[j]] <- data.frame(cbind(x, select(thisgame, DATE, travel, rest_differential, home_team_name, road_team_name, home_team_points, road_team_points), row.names=NULL), stringsAsFactors = FALSE)
+    }
+    samplesdf <- data.frame(rbindlist(samples))
+  } else{
+     df <- mutate(dist_active, share_of_minutes_signed = ifelse(OWN_TEAM==selected_team, m_share_of_minutes, -m_share_of_minutes))
+     x <- get_surplus_variables(df, nclus) %>%
+       left_join(win_perc, by="selected_team")
+     samplesdf <- data.frame(cbind(x, select(thisgame, DATE, travel, rest_differential, home_team_name, road_team_name, home_team_points, road_team_points), row.names=NULL), stringsAsFactors = FALSE)
   }
-  
-  samplesdf <- data.frame(rbindlist(samples))
 
+  rm(x)
   x <- select(samplesdf, -game_id, -DATE, -home_team_name, -road_team_name, -selected_team, -selected_team_win, -home_team_points, -road_team_points)
   f <- as.formula(~.)
   X <- model.matrix(f, x)
@@ -69,11 +82,10 @@ predict_game <- function(b, history, win_perc, id, date, runs=100, tobescored, n
   samplesdf$d_prob_selected_team_win <- ifelse(samplesdf$prob_win>.5, 1.0, 0.0)
   
   prediction <- group_by(samplesdf, game_id, DATE, home_team_name, road_team_name, selected_team, selected_team_win) %>%
-    summarise(prob_selected_team_win=mean(as.numeric(d_prob_selected_team_win))) %>%
+    summarise(prob_selected_team_win=mean(as.numeric(d_prob_selected_team_win)), 
+              selected_team_score=mean(as.numeric(prob_win))) %>%
     ungroup()
   
   return(data.frame(prediction))
   
 }
-
-

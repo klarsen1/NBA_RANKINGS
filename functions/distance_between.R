71b271ee86aee7838831c45943f75b42ad2481b3
Kim Larsen <thirdlovechangethisname@MacBook-Air-6.local>
2018-12-12 13:50:15 -0800

    getting ready for the 2018-2019 season

--- functions/distance_between.R
+++ functions/distance_between.R
@@ -2,5 +2,5 @@ distance_between <- function(long1, lat1, long2, lat2) {
   R <- 3959
   d <- acos(sin(lat1*pi/180)*sin(lat2*pi/180) + 
               cos(lat1*pi/180)*cos(lat2*pi/180) * cos(long2*pi/180-long1*pi/180)) * R
-  return(d)
+  if (is.na(d)) {return(0.00)} else {return(d)}
 }
